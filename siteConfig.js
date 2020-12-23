2990e9b4a2f28c74f18b45ac802c425fde668a18
Natty_Boh <natty.boh.torn@gmail.com>
2020-12-22 19:31:52 -0500

    test

--- siteConfig.js
+++ siteConfig.js
@@ -2,8 +2,8 @@ module.exports = {
   name: 'Final Earth Tools',
   shortName: 'FET',
   description: 'A build calculator for the game Final Earth',
-  url: 'https://natty-boh.github.io/final-earth-tools', // Domain of your site without prefix!
-  prefix: '/',
+  url: 'https://natty-boh.github.io', // Domain of your site without  prefix!
+  prefix: '/final-earth-tools/',
   author: 'Natty_Boh',
   twitter: ''
 }
