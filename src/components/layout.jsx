99e43977112ee96965e8025692215a3071e7402f
Natty_Boh <natty.boh.torn@gmail.com>
2022-03-31 22:25:15 -0400

    add tools page

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,23 +3,43 @@ import { Link } from "gatsby"
 
 const Layout = props => {
   const { title, children } = props
+  const [toggleNav, setToggleNav] = React.useState(false)
   return (
-    <div className="site-wrapper">
+    <div className={`site-wrapper ${toggleNav ? `site-head-open` : ``}`}>
       <header className="site-head">
         <div className="site-head-container">
-          <nav id="swup" className="site-head-left">
-            <ul className="nav">
-              <li className="nav-home nav-current">
+          <a
+            className="nav-burger"
+            href={`#`}
+            onClick={() => setToggleNav(!toggleNav)}
+          >
+            <div
+              className="hamburger hamburger--collapse"
+              aria-label="Menu"
+              role="button"
+              aria-controls="navigation"
+            >
+              <div className="hamburger-box">
+                <div className="hamburger-inner" />
+              </div>
+            </div>
+          </a>
+          <nav id="swup" class="site-head-left">
+          <ul className="nav">
+              <li className="nav-elements">
                 <Link to={`/`}>Home</Link>
               </li>
-              <li className="nav-about">
+              <li className="nav-elements">
                 <Link to={`/axis`}>Axis Calculator</Link>
               </li>
               <li className="nav-elements">
                 <Link to={`/allies`}>Allies Calculator</Link>
               </li>
               <li className="nav-elements">
-                <Link to={`/networth`}>Networth Calculator</Link>
+                <Link to={`/networth`}>Networth </Link>
+              </li>
+              <li className="nav-elements">
+                <Link to={`/tools`}>Tools</Link>
               </li>
             </ul>
           </nav>
@@ -27,6 +47,8 @@ const Layout = props => {
             <Link className="site-head-logo" to={`/`}>
               {title}
             </Link>
+          </div>
+          <div className="site-head-right">
           </div>
         </div>
       </header>
